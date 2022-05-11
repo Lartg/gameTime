a97cfefc28ef4e4050a91db0e5766cf725efd675
Lartg <roc.gerrety@gmail.com>
2022-05-11 12:05:17 -0700

    added some style to events

--- controllers/games.js
+++ controllers/games.js
@@ -1,6 +1,28 @@
 module.exports = function (app, models) {
 
 app.get('/', (req, res) => {
-  res.render('games-index')
+  games = {
+    1:{
+    id: 'test',
+    title: 'Title',
+    imgUrl: ''
+    },
+    2:
+    {
+      id: 'test2',
+      title: 'Title2',
+      imgUrl: ''
+      },
+  }
+  res.render('games-index', {games: games})
 })
+app.get('/games/:id', (req, res) => {
+  events = 5
+  res.render('events-index', {events: events})
+})
+
+
+
+
 }
+
